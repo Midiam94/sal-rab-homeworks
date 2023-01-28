4fba4d08032ca8c9a2ad1768b1e79b169236e7b4
Молчанова Юлия <molchanchik94@mail.ru>
2023-01-28 13:49:38 +0300

    change6

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,9 +1,7 @@
+    // Задание №1.1. Оповещение о добавлении в корзину
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-
-    // Задание №1.1. Оповещение о добавлении в корзину
-
     let message;
     message = productName;
     message = message + " ";
@@ -13,32 +11,19 @@ function addInCartMessage(title, price) {
     message = message + " ";
     message = message + "теперь в корзине!";
     console.log(message);
-   
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message значение переменной productPrice
-    // добавьте в конец переменной message пробел
-    // добавьте в конец переменной message текст 'теперь в корзине!'
-    // выведите переменную message в консоль
-    // Конец решения задания №1.1.
-
     return message;
 }
 
-function addInCartCountChange(value) {
 
-    // Задание №1.2. Увеличение количества в корзине
+// Задание №1.2. Увеличение количества в корзине
+function addInCartCountChange(value) {
     let oldValue = value;
     let newValue;
     newValue += 1;
     console.log(newValue);
-    
-    // создайте переменную newValue
-    // присвойте ей значение oldValue
-    // увеличьте значение newValue на 1
-    // Конец решения задания №1.2.
-
     return newValue;
 }
+
 // Задание №1.3. Увеличение суммы в корзине
     function addInCartSumChange(sum, delta) {
     let oldSum = sum;
@@ -47,12 +32,6 @@ function addInCartCountChange(value) {
     let newSumText = newSum + "₽";
     newSumText = "${newSum} ₽"; 
     console.log(newSum);
-
-    // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    // замените склеивание, используемое в newSum Text на шаблонную строку
-    // Конец решения задания №1.3.
-
     return newSumText;
 }
 
